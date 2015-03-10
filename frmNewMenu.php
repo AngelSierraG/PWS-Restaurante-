a70adb46dd90891dbd92dbf0bcd516ddc2a4ab22
hilsier <hilsier4simpleplan@gmail.com>
2015-03-10 17:22:18 -0600

    links cambiados css

--- frmNewMenu.php
+++ frmNewMenu.php
@@ -5,17 +5,17 @@
 	<title>Admin PWS-Restaurante</title>
 	
 	
-	<link rel="stylesheet" href="../view/css/layout.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="../view/css/calendarview.css">
+	<link rel="stylesheet" href="view/css/layout.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="view/css/calendarview.css">
 
 	<!--[if lt IE 9]>
 	<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	<script src="../view/js/jquery-1.5.2.min.js" type="text/javascript"></script>
-	<script src="../view/js/hideshow.js" type="text/javascript"></script>
-	<script src="../view/js/jquery.tablesorter.min.js" type="text/javascript"></script>
-	<script type="text/javascript" src="../view/js/jquery.equalHeight.js"></script>
+	<script src="view/js/jquery-1.5.2.min.js" type="text/javascript"></script>
+	<script src="view/js/hideshow.js" type="text/javascript"></script>
+	<script src="view/js/jquery.tablesorter.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="view/js/jquery.equalHeight.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function() 
     	{ 
@@ -186,9 +186,9 @@
 		<div class="spacer"></div>
 	</section>
 
-	<script src="../view/js/prototype.js"></script>
-    <script src="../view/js/calendarview.js"></script>
-    <script src="../view/js/Functions.js"></script>
+	<script src="view/js/prototype.js"></script>
+    <script src="view/js/calendarview.js"></script>
+    <script src="view/js/Functions.js"></script>
 </body>
  	
 
